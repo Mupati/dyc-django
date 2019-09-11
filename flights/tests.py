6c1bb53e049d586c5539b0b2928f3a7911042045
Mupati <scientificgh@gmail.com>
2019-09-11 19:46:11 +0000

    Remove unneeded tests

--- flights/tests.py
+++ flights/tests.py
@@ -43,11 +43,11 @@ class ModelsTestCase(TestCase):
         f = Flight.objects.get(origin=a1, destination=a2, duration=-100)
         self.assertFalse(f.is_valid_flight())
 
-    def test_index(self):
-        c = Client()
-        response = c.get("/")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context["flights"].count(), 2)
+    # def test_index(self):
+    #     c = Client()
+    #     response = c.get("/")
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(response.context["flights"].count(), 2)
 
     # def test_valid_flight_page(self):
     #     a1 = Airport.objects.get(code="AAA")
