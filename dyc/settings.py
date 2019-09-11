0ba2b99fb907be5ef5c97326ed43782cbfb21d42
Mupati <scientificgh@gmail.com>
2019-09-11 21:47:27 +0000

    Edit settings

--- dyc/settings.py
+++ dyc/settings.py
@@ -83,10 +83,10 @@ DATABASES = {
         'HOST': 'db',
         'PORT': 5432,
     },
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'sqlite': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
